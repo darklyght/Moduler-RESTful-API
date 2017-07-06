42f70b7899c9c97a79bf3f62fbe6396a02c4ac9a
Kat Li Yang <kat.liyang@gmail.com>
2017-07-06 10:36:59 +0800

    Add module add GET and POST

--- routes/routes.js
+++ routes/routes.js
@@ -28,6 +28,21 @@ const auth = function(req, res, next) {
   })
 }
 
+var decode_grade = {
+  'Aplus': 'A+',
+  'A': 'A',
+  'Aminus': 'A-',
+  'Bplus': 'B+',
+  'B': 'B',
+  'Bminus': 'B-',
+  'Cplus': 'C+',
+  'C': 'C',
+  'Dplus': 'D+',
+  'F': 'F',
+  'CS': 'CS',
+  'CU': 'CU'
+};
+
 var appRouter = function(app) {
   app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
@@ -36,6 +51,254 @@ var appRouter = function(app) {
     next();
   });
   app.get('/users/modules', auth, function(req, res) {
+    app_users.find(username).fetch().subscribe(result => {
+      res.json(result.modules);
+    }, err => {
+      console.log(err);
+      res.send(err);
+    });
+  });
+  app.post('/users/modules', auth, function(req, res) {
+    app_users.find(username).fetch().subscribe(result => {
+      res.json(result.modules);
+    }, err => {
+      console.log(err);
+      res.send(err);
+    });
+  });
+  app.get('/users/modules/add', auth, function(req, res) {
+    if (!req.query.semester) {
+      return res.send({
+        'status': 'error',
+        'message': 'Missing semester'
+      });
+    }
+    if (!req.query.code) {
+      return res.send({
+        'status': 'error',
+        'message': 'Missing module code'
+      });
+    }
+    if (req.query.credits && req.query.credits !== parseInt(credits)) {
+      return res.send({
+        'status': 'error',
+        'message': 'Invalid module credits'
+      });
+    }
+    if (req.query.type && req.query.type !== 'ULR' && req.query.type !== 'FLR' && req.query.type !== 'RCM' && req.query.type !== 'TE' && req.query.type !== 'IA' && req.query.type !== 'UE') {
+      return res.send({
+        'status': 'error',
+        'message': 'Invalid module type'
+      });
+    }
+    if (req.query.grade && req.query.grade !== 'Aplus' && req.query.grade !== 'A' && req.query.grade !== 'Aminus' && req.query.grade !== 'Bplus' && req.query.grade !== 'B' && req.query.grade !== 'Bminus' && req.query.grade !== 'Cplus' && req.query.grade !== 'C' && req.query.grade !== 'Dplus' && req.query.grade !== 'D' && req.query.grade !== 'F' && req.query.grade !== 'CS' && req.query.grade !== 'CU') {
+      return res.send({
+        'status': 'error',
+        'message': 'Invalid module grade'
+      });
+    }
+    if (req.query.su_option && req.query.su_option !== 'true' && req.query.su_option !== 'false') {
+      return res.send({
+        'status': 'error',
+        'message': 'Invalid module S/U option'
+      });
+    }
+    app_users.find(username).fetch().subscribe(result => {
+      var modules = result.modules;
+      while (modules.length < req.query.semester) {
+        modules.push([])
+      }
+      for (var i = 0; i < modules.length; i++) {
+        for (var j = 0; j < modules[i].length; j++) {
+          if (req.query.code === modules[i][j].code) {
+            return res.send({
+              'status': 'error',
+              'message': 'Module code already exists'
+            })
+          }
+        }
+      }
+      var new_module = {
+        'code': req.query.code,
+        'credits': 4,
+        'type': 'ULR',
+        'grade': 'Planned',
+        'su_option': false
+      };
+      if (req.query.credits) {
+        new_module.credits = req.query.credits;
+      }
+      if (req.query.type) {
+        new_module.type = req.query.type;
+      }
+      if (req.query.grade) {
+        new_module.grade = decode_grade[req.query.grade];
+      }
+      if (req.query.su_option) {
+        new_module.su_option = (req.query.su_option === 'true');
+      }
+      if (new_module.su_option === true) {
+        if (new_module.grade === 'D' || new_module.grade === 'D+' || new_module.grade === 'F') {
+          new_module.final_grade = 'U';
+        }
+        else {
+          new_module.final_grade = 'S';
+        }
+      }
+      else {
+        new_module.final_grade = new_module.grade;
+      }
+      modules[req.query.semester-1].push(new_module)
+      app_users.update({
+        id: username,
+        modules: modules
+      });
+      res.send({
+        'status': 'success',
+        'message': modules
+      });
+    }, err => {
+      console.log(err);
+      res.send(err);
+    });
+  });
+  app.get('/users/modules', auth, function(req, res) {
+    app_users.find(username).fetch().subscribe(result => {
+      res.json(result.modules);
+    }, err => {
+      console.log(err);
+      res.send(err);
+    });
+  });
+  app.post('/users/modules', auth, function(req, res) {
+    app_users.find(username).fetch().subscribe(result => {
+      res.json(result.modules);
+    }, err => {
+      console.log(err);
+      res.send(err);
+    });
+  });
+  app.post('/users/modules/add', auth, function(req, res) {
+    if (!req.query.semester) {
+      return res.send({
+        'status': 'error',
+        'message': 'Missing semester'
+      });
+    }
+    if (!req.query.code) {
+      return res.send({
+        'status': 'error',
+        'message': 'Missing module code'
+      });
+    }
+    if (req.query.credits && req.query.credits !== parseInt(credits)) {
+      return res.send({
+        'status': 'error',
+        'message': 'Invalid module credits'
+      });
+    }
+    if (req.query.type && req.query.type !== 'ULR' && req.query.type !== 'FLR' && req.query.type !== 'RCM' && req.query.type !== 'TE' && req.query.type !== 'IA' && req.query.type !== 'UE') {
+      return res.send({
+        'status': 'error',
+        'message': 'Invalid module type'
+      });
+    }
+    if (req.query.grade && req.query.grade !== 'Aplus' && req.query.grade !== 'A' && req.query.grade !== 'Aminus' && req.query.grade !== 'Bplus' && req.query.grade !== 'B' && req.query.grade !== 'Bminus' && req.query.grade !== 'Cplus' && req.query.grade !== 'C' && req.query.grade !== 'Dplus' && req.query.grade !== 'D' && req.query.grade !== 'F' && req.query.grade !== 'CS' && req.query.grade !== 'CU') {
+      return res.send({
+        'status': 'error',
+        'message': 'Invalid module grade'
+      });
+    }
+    if (req.query.su_option && req.query.su_option !== 'true' && req.query.su_option !== 'false') {
+      return res.send({
+        'status': 'error',
+        'message': 'Invalid module S/U option'
+      });
+    }
+    app_users.find(username).fetch().subscribe(result => {
+      var modules = result.modules;
+      while (modules.length < req.query.semester) {
+        modules.push([])
+      }
+      for (var i = 0; i < modules.length; i++) {
+        for (var j = 0; j < modules[i].length; j++) {
+          if (req.query.code === modules[i][j].code) {
+            return res.send({
+              'status': 'error',
+              'message': 'Module code already exists'
+            })
+          }
+        }
+      }
+      var new_module = {
+        'code': req.query.code,
+        'credits': 4,
+        'type': 'ULR',
+        'grade': 'Planned',
+        'su_option': false
+      };
+      if (req.query.credits) {
+        new_module.credits = req.query.credits;
+      }
+      if (req.query.type) {
+        new_module.type = req.query.type;
+      }
+      if (req.query.grade) {
+        new_module.grade = decode_grade[req.query.grade];
+      }
+      if (req.query.su_option) {
+        new_module.su_option = (req.query.su_option === 'true');
+      }
+      if (new_module.su_option === true) {
+        if (new_module.grade === 'D' || new_module.grade === 'D+' || new_module.grade === 'F') {
+          new_module.final_grade = 'U';
+        }
+        else {
+          new_module.final_grade = 'S';
+        }
+      }
+      else {
+        new_module.final_grade = new_module.grade;
+      }
+      modules[req.query.semester-1].push(new_module)
+      app_users.update({
+        id: username,
+        modules: modules
+      });
+      res.send({
+        'status': 'success',
+        'message': modules
+      });
+    }, err => {
+      console.log(err);
+      res.send(err);
+    });
+  });
+  app.get('/users/modules/update', auth, function(req, res) {
+    app_users.find(username).fetch().subscribe(result => {
+      res.json(result.modules);
+    }, err => {
+      console.log(err);
+      res.send(err);
+    });
+  });
+  app.post('/users/modules/update', auth, function(req, res) {
+    app_users.find(username).fetch().subscribe(result => {
+      res.json(result.modules);
+    }, err => {
+      console.log(err);
+      res.send(err);
+    });
+  });
+  app.get('/users/modules/delete', auth, function(req, res) {
+    app_users.find(username).fetch().subscribe(result => {
+      res.json(result.modules);
+    }, err => {
+      console.log(err);
+      res.send(err);
+    });
+  });
+  app.post('/users/modules/delete', auth, function(req, res) {
     app_users.find(username).fetch().subscribe(result => {
       res.json(result.modules);
     }, err => {
